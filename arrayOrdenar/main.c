c4b21602864b7219f914283bcfa1b292783f0704
DBulacio <44949670+DBulacio@users.noreply.github.com>
2019-09-09 10:40:34 -0300

    Agregar a libreria.
    Ordenar array de manera descendente y ascendente

--- arrayOrdenar/main.c
+++ arrayOrdenar/main.c
@@ -4,36 +4,23 @@
 #define TAM 5
 
 void mostrarVectorInt(int vec[], int tam);
+void ordenarVectorInt(int vec[], int tam, int criterio);
 
 int main()
 {
-
+    int crit;
     int vec[TAM] = {7, 5, 8, 1, 3};
-    int aux;
 
     printf("Vector sin ordenar\n\n");
-
     mostrarVectorInt(vec, TAM);
 
+    printf("Ingrese 0 para ascendente y 1 para descendente: ");
+    scanf("%d", &crit);
 
-
-    for(int i = 0; i < TAM-1; i++){
-
-        for(int j = i + 1; j < TAM; j++){
-
-            if( vec[j] < vec[i]){
-
-                aux = vec[j];
-                vec[j] = vec[i];
-                vec[i] = aux;
-            }
-        }
-    }
+    ordenarVectorInt(vec, TAM, crit);
 
     printf("Vector Ordenado\n\n");
-
     mostrarVectorInt(vec, TAM);
-
 
     return 0;
 }
@@ -44,4 +31,31 @@ void mostrarVectorInt(int vec[], int tam){
         printf("%d ", vec[i]);
     }
     printf("\n\n");
+}
+
+void ordenarVectorInt(int vec[], int tam, int criterio){
+    int aux;
+    if(criterio == 1){      //ascendente
+        printf("CRITERIO DESCENDENTE\n\n");
+        for(int i = 0; i < TAM-1; i++){
+            for(int j = i + 1; j < TAM; j++){
+                if( vec[j] < vec[i]){
+                    aux = vec[j];
+                    vec[j] = vec[i];
+                    vec[i] = aux;
+                }
+            }
+        }
+    } else {                // descendente
+        printf("CRITERIO ASCENDENTE\n\n");
+        for(int i = 0; i < TAM-1; i++){
+            for(int j = i + 1; j < TAM; j++){
+                if( vec[j] > vec[i]){
+                    aux = vec[j];
+                    vec[j] = vec[i];
+                    vec[i] = aux;
+                }
+            }
+        }
+    }
 }
